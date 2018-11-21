2dfa98c24ac83dac760f96a47a6ce75cf71d933f
Xiaocong Dong <15652914852@163.com>
2018-11-21 23:15:49 +0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'none',
-    entry: './src/index.js'
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 }
